70cc3f339707ceafa2d3a4d959f9457ae939d2bb
valeriis <valerii.shkolnyi@playtech.com>
2021-10-13 19:27:13 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '033e3ad5972a86fa6d40e818fcbf3f83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd07f1ef5-e39b-488f-8f3a-0e526d027e39'
+};
