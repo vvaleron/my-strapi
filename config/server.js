4a1cd978f0d0c0d541b041714f5892b3e51beafa
Valeriy <vvaleron@users.noreply.github.com>
2021-10-16 14:52:53 +0300

    prod

--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  "autoReload": {
+    "enabled": false
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '033e3ad5972a86fa6d40e818fcbf3f83'),
