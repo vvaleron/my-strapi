d82ca848d761bb7cf4dcab2da32400f22e4d01e3
Valeriy <vvaleron@users.noreply.github.com>
2022-01-10 01:22:42 +0200

    add

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,3 @@
-module.exports = (env) => ({
-    "backup-restore": {
-        postgres: {
-            // Update with your path
-            pathToPgDump: "/usr/local/opt/libpq/bin/pg_dump",
-        },
-    },
-    email: {
-        provider: 'nodemailer',
-        providerOptions: {
-            host: env('SMTP_HOST', 'smtp.example.com'),
-            port: env('SMTP_PORT', 587),
-            auth: {
-                user: env('SMTP_USERNAME'),
-                pass: env('SMTP_PASSWORD'),
-            },
-            // ... any custom nodemailer options
-        },
-        settings: {
-            defaultFrom: 'hello@example.com',
-            defaultReplyTo: 'hello@example.com',
-        },
-    }
+module.exports = () => ({
+    
 });
