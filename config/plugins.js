fc1b371c1dd4114d6d62787921148061b8f6ce4c
Valeriy <vvaleron@users.noreply.github.com>
2022-01-10 00:48:15 +0200

    strapi

--- config/plugins.js
+++ config/plugins.js
@@ -5,4 +5,20 @@ module.exports = () => ({
             pathToPgDump: "/usr/local/opt/libpq/bin/pg_dump",
         },
     },
+    email: {
+        provider: 'nodemailer',
+        providerOptions: {
+            host: env('SMTP_HOST', 'smtp.example.com'),
+            port: env('SMTP_PORT', 587),
+            auth: {
+                user: env('SMTP_USERNAME'),
+                pass: env('SMTP_PASSWORD'),
+            },
+            // ... any custom nodemailer options
+        },
+        settings: {
+            defaultFrom: 'hello@example.com',
+            defaultReplyTo: 'hello@example.com',
+        },
+    }
 });
